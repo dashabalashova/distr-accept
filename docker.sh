2e4eee318baf15fe71cf42eae8927daff7d5773d
dashabalashova <dashabalashova@gmail.com>
2025-10-30 19:09:02 +0100

    SLURM_SUBMIT_DIR

--- docker.sh
+++ docker.sh
@@ -2,10 +2,9 @@
 set -euo pipefail
 
 echo "Launch on root@worker-0 ..."
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@worker-0 bash -s -- "$SLURM_SUBMIT_DIR" <<'REMOTE_EOF'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@worker-0 bash <<'REMOTE_EOF'
 set -euo pipefail
-SLURM_SUBMIT_DIR="$1"
-cd "$SLURM_SUBMIT_DIR"
+cd /root/distr-accept/
 echo "Building docker image in $(pwd)..."
 docker build -t deepspeed-train:latest .
 enroot import --output deepspeed-train.sqsh dockerd://deepspeed-train:latest
--- push_container.sh
+++ push_container.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 GHCR_IMAGE="${GHCR_IMAGE:-ghcr.io/dashabalashova/deepspeed-train:latest}"
 
 echo "Pushing deepspeed-train:latest -> $GHCR_IMAGE on root@worker-0"
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@worker-0 "cd '$SLURM_SUBMIT_DIR' \
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@worker-0 "cd /root/distr-accept/ \
   && docker tag deepspeed-train:latest '$GHCR_IMAGE' \
   && docker push '$GHCR_IMAGE'"
